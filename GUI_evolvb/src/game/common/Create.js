66a6acff03656e228b949344f9e2486ab069c453
PZO <piotr.zoltaszek@capgemini.com>
2020-11-28 16:22:27 +0100

    Add scrolling

--- GUI_evolvb/src/game/common/Create.js
+++ GUI_evolvb/src/game/common/Create.js
@@ -35,11 +35,12 @@ const Create = {
         frame.appendChild(playerDom);
         return playerDom;
     },
-    actor: async(frame, levelActors, actorClickHandler) => {
+    actor: async(frame, actorsObj, levelActors, actorClickHandler) => {
         levelActors.forEach(el => {
             let newEl = document.createElement('div');
             newEl.className = `${el.type}`;
             newEl.id = `${el.sub}`;
+            newEl.actorId= el.actorId;
             newEl.style.width = `${el.width * scale}px`;
             newEl.style.height = `${el.height * scale}px`;
             newEl.style.left = `${el.posX * scale}px`;
@@ -48,6 +49,15 @@ const Create = {
             newEl.style.position = "absolute"
             newEl.addEventListener("click", actorClickHandler);
             frame.appendChild(newEl);
+            actorsObj.push({
+                type: el.type,
+                actorId: el.actorId,
+                id: el.sub,
+                x0: el.posX * scale,
+                x1: el.posX * scale + el.width * scale,
+                y0: el.posY * scale,
+                y1: el.posY * scale + el.height * scale
+            })
         });
     },
 }
